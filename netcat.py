739edf60cf4c27f12a670e6f4a003b7b1fe6c26c
Per-Åke Pettersson <79310855+PerAkeP@users.noreply.github.com>
2024-09-10 20:59:48 +0200

    final NetCat

--- netcat.py
+++ netcat.py
@@ -18,9 +18,85 @@ class NetCat:
         def __init__(self, args, buffer=None):
             self.args = args
             self.buffer = buffer
+            self.socket = socket.socket(socket.AF, socket.SOCK_STREAM)
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         def run(self):
-            pass
+            if self.args.listen:
+                 self.listen()
+            else:
+                 self.send()
+
+        def send(self):
+             self.socket.connect((self.args.target, self.args.port))
+             if self.buffer:
+                  self.socket.send(self.buffer)
+
+             try:
+                 while True:
+                      recv_len = 1
+                      response = ''
+                      while recv_len:
+                           data = self.socket.recv(4096)
+                           recv_len = len(data)
+                           response += data.decode()
+                           if recv_len < 4096:
+                                break
+                           if response:
+                                print(response)
+                                buffer = input('> ')
+                                buffer += '\n'
+                                self.socket.send(buffer.encode()) 
+             except KeyboardInterrupt:
+                  print('User terminated!')
+                  self.socket.close()
+                  sys.exit()
+        
+        def listen(self):
+             self.socket.bind((self.args.target, self.args.port))
+             self.socket.listen(5)
+
+             while True:
+                  client_socket, _ = self.socket.accept()
+                  client_thread = threading.Thread(
+                       target=self.handle, args=(client_socket,)
+                  )
+                  client_thread.start()
+
+        def handle(self, client_soocket):
+             if self.args.execute:
+                  output = execute(self.args.execute)
+                  client_soocket.send(output.encode())
+            
+             elif self.args.upload:
+                 file_buf = b''
+                 while True:
+                      data = client_soocket.recv(4096)
+                      if data:
+                           file_buf += data
+                      else:
+                           break
+
+                 with open(self.args.upload, 'wb') as file:
+                      file.write(file_buf)
+                 message = f'Saved file {self.args.upload}'
+                 client_soocket.send(message.encode())
+
+             elif self.args.command:
+                  cmd_buf = b''
+                  while True:
+                       try:
+                            client_soocket.send(b'BHP: #> ')
+                            while '\n' not in cmd_buf.decode():
+                                 cmd_buf += client_soocket.recv(64)
+                            response = execute(cmd_buf.decode())
+                            if response:
+                                client_soocket.send(response.encode())
+                            cmd_buf = b''
+                       except Exception as e:
+                            print(f'server killed {e}')
+                            self.socket.close()
+                            sys.exit()    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
